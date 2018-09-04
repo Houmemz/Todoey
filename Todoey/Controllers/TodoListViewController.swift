9a5bf2fb8e32696e270c0ff6b7f1a0288d1a14dc
Houmem Zaghdoudi <houmemzaghdoudi@hotmail.com>
2018-09-04 12:10:21 +0100

    Convert to MVC design pattern.

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,16 +10,32 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogogon"]
-    
+    var itemArray = [Item]()
+    // reusable cell methond makes the check mark reappear at the bottom whent he cell disappear on the top of the screen
+    // this is becuase the checkmark is cell property, we need to assciate the checkmark with the data in the cell not the cell
+    // instead of array we can use a Dictionnay, a better solution is to create a data model
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroyer"
+        itemArray.append(newItem3)
+        
+        
         // enables the app to look into the app sandbox for the simulator and garb data on load
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
+          itemArray = items
+            
         }
     }
 
@@ -32,7 +48,21 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        //Ternery operator :
+        // value = condition ? valueifTrue : valueiffalse
+        
+        cell.accessoryType = item.done  ? .checkmark : .none
+        // the above line replaces the below block of code
+        //if item.done == true {
+         //   cell.accessoryType = .checkmark
+        //} else {
+          //  cell.accessoryType = .none
+        //}
         
         return cell
         
@@ -43,13 +73,18 @@ class TodoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-         tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-         tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        // this single line above equals the below toggle cod block
+        //if itemArray[indexPath.row].done == false {
+        //    itemArray[indexPath.row].done = true
+        //} else {
+        //    itemArray[indexPath.row].done = false
+        //}
         
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        tableView.reloadData()
     
     }
     
@@ -62,7 +97,11 @@ class TodoListViewController: UITableViewController {
         let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            self.itemArray.append(textField.text!)
+            
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,16 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Houmem Zaghdoudi on 9/4/18.
+//  Copyright © 2018 Houmem Zaghdoudi. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
